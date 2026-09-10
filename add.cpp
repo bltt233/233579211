a6113f91fb31df02a19613c5137a4b29c0d8dd91
bltt233 <2086858381@qq.com>
2026-09-10 19:08:06 +0800

    2.0!

--- add.cpp
+++ add.cpp
@@ -3,6 +3,8 @@ using namespace std
 
 int main()
 {
-  cout<<"Man!What can I say"<<endl;
+  cout<<"Man!What can I say!"<<endl;
+  cout<<"-------------------------"<<endl;
+  cout<<"kskbl,zdjd?wqzdkbl!"<<endl;
   return 0;
 }
